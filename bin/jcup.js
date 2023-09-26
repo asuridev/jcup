b54dad1edae535d66939335db899c0413d92ca8f
antonio suarez <antoniosuarezrincon@gmail.com>
2023-09-26 12:53:51 -0500

    implementacion de add y remove de dependencias

--- bin/jcup.js
+++ bin/jcup.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-const yargs = require('yargs/yargs')(process.argv.slice(2))
-const { newProyect } = require('../lib/commands/new')
-const { generate } = require('../lib/commands/generate')
+const yargs = require('yargs/yargs')(process.argv.slice(2));
+const { newProyect } = require('../lib/commands/new');
+const { generate } = require('../lib/commands/generate');
+const { install } = require('../lib/commands/install')
+const { uninstall } = require('../lib/commands/uninstall')
 
 yargs.command({
   command:'new <name>',
@@ -10,6 +12,26 @@ yargs.command({
   handler: newProyect
 }).positional('name',{
   describe: 'nombre del proyecto',
+  type: 'string',
+});
+
+yargs.command({
+  command:'uninstall <nameDependencie>',
+  aliases:['rm'],
+  describe:'Elimina una dependencia que haya sido instalada',
+  handler: uninstall
+}).positional('nameDependencie',{
+  describe: 'nombre de la dependencies',
+  type: 'string',
+});
+
+yargs.command({
+  command:'install [newDependencie]',
+  aliases:['i'],
+  describe:'Instala una dependencia que se encuente en jcup',
+  handler: install
+}).positional('newDependencie',{
+  describe: 'nombre de la nueva dependencies',
   type: 'string',
 });
 
